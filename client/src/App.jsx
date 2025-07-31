08e2c981b7c75534cd19d56d1bc955cd9bd87ef3
ronishastha495 <shrestharonisha374@gmail.com>
2025-07-31 23:38:55 +0545

    feat(client): initialize React app with Vite and Tailwind CSS
    - Added package.json with dependencies for React, Tailwind CSS, and Vite.
    - Included Vite logo SVG in public directory.
    - Created main application component (App.jsx) with a simple layout.
    - Added React logo SVG in assets directory.
    - Set up global CSS file (index.css) to import Tailwind CSS.
    - Configured entry point (main.jsx) for rendering the React application.
    - Created Vite configuration file (vite.config.js) with React and Tailwind CSS plugins.

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+       <>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-600">Xpenso App 🚀</h1>
+   </div>
+    </>
+   
+  );
+}
+
+export default App

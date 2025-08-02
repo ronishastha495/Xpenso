7de4b5d117aa65a7857765413fe4e754c0ed7afd
ronishastha495 <shrestharonisha374@gmail.com>
2025-08-02 23:14:17 +0545

    feat(settings): switch database from SQLite to PostgreSQL with configuration

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xpenso',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',  # or your DB server IP
+        'PORT': '5432',       # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
